8968b11be7368c7b96f5fc6df5ca3de5512a76c4
Brad Ruud <bradruud@gmail.com>
2020-02-06 15:46:01 +0000

    Initial commit with all of my work so far

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math (a, b, c) {
+return solution = b * c + a 
+}
+console.log(math(53, 61, 67))
---
+++ if-statement.js
@@ -0,0 +1,8 @@
+let fruit = 'orange'
+namelength = fruit.length
+
+if (namelength > 5) {
+  console.log("The fruit name has more than five characters.")
+} else {
+  console.log("The fruit name has five characters or less")
+}
